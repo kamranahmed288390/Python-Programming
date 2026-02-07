3913c727c801275751f8e08c9be655708673095b
kamranahmed288390 <150603111+kamranahmed288390@users.noreply.github.com>
2026-02-07 15:30:14 -0500

    update project

---
+++ Mega Project 1-Jarvis/main.py
@@ -0,0 +1,36 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+
+recognizer =  sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+if  __name__ == "__main__":
+    speak("Intializing Jarvis...")
+    while True:
+    # Listen for the wake word "Jarvis"
+    # Obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Listening..")
+            audio = r.listen(source)
+
+        try:
+            command = r.recognize_sphinx(audio)
+            print(command)
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}" .format(e))
+
+
+    # Recognize speech using Sphinx
+    
+
+    
+   
